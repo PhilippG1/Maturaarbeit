1402785d4de41d7686615fcdeefa1ed080b26ac1
PhilippG1 <philipp.gempp@gmx.ch>
2022-12-11 20:11:39 +0100

    Einleitung und Vorgehen Schriftlicher Teil

--- My Game/Assets/Scripts/InteractableObjects/Bounce.cs
+++ My Game/Assets/Scripts/InteractableObjects/Bounce.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Bounce : MonoBehaviour
 {
     // Start is called before the first frame update
+    private bool bounce;
+    private GameObject collided;
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Player"))
@@ -12,10 +15,26 @@ public class Bounce : MonoBehaviour
             
             if ((collision.transform.position.y - transform.position.y )> 0)
             {
-                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1000));
-                Debug.Log("asfasdfasdf");
+                collided = collision.gameObject;
+                bounce = true;
+                StartCoroutine(StopBounce());
+                
             }
         }
+        
     }
-
+    private void Update()
+    {
+        if (bounce == true)
+        {
+            collided.GetComponent<Rigidbody2D>().velocity = new Vector2(collided.GetComponent<Rigidbody2D>().velocity.x, 25);
+            
+        }
+    }
+    private IEnumerator StopBounce()
+    {
+        yield return new WaitForSeconds(0.5f);
+        bounce = false;
+        collided.GetComponent<Rigidbody2D>().velocity = new Vector2(collided.GetComponent<Rigidbody2D>().velocity.x, 10);
+    }
 }
