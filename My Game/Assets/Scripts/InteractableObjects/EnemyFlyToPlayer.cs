34fef6001626d0294185cc84c94aff28214d97de
PhilippG1 <philipp.gempp@gmx.ch>
2022-12-11 22:21:30 +0100

    Gegner folgt Spieler

---
+++ My Game/Assets/Scripts/InteractableObjects/EnemyFlyToPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlyToPlayer : MonoBehaviour
+{
+    private GameObject PlayerObject;
+    private Vector2 direction;
+    [SerializeField]private float speed = 2;
+
+    void Start()
+    {
+        PlayerObject = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = (PlayerObject.transform.position - transform.position).normalized;
+        this.gameObject.GetComponent<Rigidbody2D>().velocity = direction;
+    }
+}
